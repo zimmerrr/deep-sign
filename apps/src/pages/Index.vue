d594a9e21dc106abd7f0336a15aa409b99bc1b0f
zimmer <zimmertorrijos@gmail.com>
2024-06-02 23:24:38 +0800

    apply components on index

--- apps/src/pages/Index.vue
+++ apps/src/pages/Index.vue
@@ -15,7 +15,11 @@
             v-if="menu"
             class="q-my-md text-center text-white text-h4"
           >
-            {{ header }}
+            <gestures v-if="mode === Mode.gestures" />
+            <text-to-gesture v-if="mode === Mode.text_to_gesture" />
+            <gesture-to-text v-if="mode === Mode.gesture_to_text" />
+            <Upload v-if="mode === Mode.upload" />
+            <Settings v-if="mode === Mode.settings" />
           </div>
         </div>
       </q-card-section>
@@ -27,8 +31,26 @@
 <script lang="ts" setup>
 import Header from 'src/components/Header.vue'
 import Footer from 'src/components/Footer.vue'
+import Gestures from 'src/components/Gestures.vue'
+import TextToGesture from 'src/components/TextToGesture.vue'
+import GestureToText from 'src/components/GestureToText.vue'
+import Upload from 'src/components/Upload.vue'
+import Settings from 'src/components/Settings.vue'
 import { MENU } from 'src/components/constants'
 import { ref } from 'vue'
+
+enum Mode {
+  loading,
+  gestures,
+  text_to_gesture,
+  gesture_to_text,
+  upload,
+  settings,
+}
+
+const hasModel = ref(true)
+const mode = ref<Mode>(hasModel.value ? Mode.gestures : Mode.loading)
+const lastScannerMode = ref(0)
 
 const menu = ref({})
 const header = ref('Gesture')
@@ -38,6 +60,12 @@ function onUpdateMenu(option: any) {
   header.value = option.header
   subheader.value = option.subheader
   menu.value = option
+  setMode(Mode[option.key as keyof typeof Mode])
+}
+
+function setMode(newMode: Mode) {
+  lastScannerMode.value = newMode
+  mode.value = newMode
 }
 </script>
 
