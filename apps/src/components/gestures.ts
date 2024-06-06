c269a00a96760dac211d780346343662cd2cdff3
zimmer <zimmertorrijos@gmail.com>
2024-06-07 01:50:38 +0800

    added gesture images mapping

---
+++ apps/src/components/gestures.ts
@@ -0,0 +1,106 @@
+export const GESTURES = [
+  {
+    name: 'A',
+    src: '/alphabet/A.png',
+  },
+  {
+    name: 'B',
+    src: '/alphabet/B.png',
+  },
+  {
+    name: 'C',
+    src: '/alphabet/C.png',
+  },
+  {
+    name: 'D',
+    src: '/alphabet/D.png',
+  },
+  {
+    name: 'E',
+    src: '/alphabet/E.png',
+  },
+  {
+    name: 'F',
+    src: '/alphabet/F.png',
+  },
+  {
+    name: 'G',
+    src: '/alphabet/G.png',
+  },
+  {
+    name: 'H',
+    src: '/alphabet/H.png',
+  },
+  {
+    name: 'I',
+    src: '/alphabet/I.png',
+  },
+  {
+    name: 'J',
+    src: '/alphabet/J.png',
+  },
+  {
+    name: 'K',
+    src: '/alphabet/K.png',
+  },
+  {
+    name: 'L',
+    src: '/alphabet/L.png',
+  },
+  {
+    name: 'M',
+    src: '/alphabet/M.png',
+  },
+  {
+    name: 'N',
+    src: '/alphabet/N.png',
+  },
+  {
+    name: 'O',
+    src: '/alphabet/O.png',
+  },
+  {
+    name: 'P',
+    src: '/alphabet/P.png',
+  },
+  {
+    name: 'Q',
+    src: '/alphabet/Q.png',
+  },
+  {
+    name: 'R',
+    src: '/alphabet/R.png',
+  },
+  {
+    name: 'S',
+    src: '/alphabet/S.png',
+  },
+  {
+    name: 'T',
+    src: '/alphabet/T.png',
+  },
+  {
+    name: 'U',
+    src: '/alphabet/U.png',
+  },
+  {
+    name: 'V',
+    src: '/alphabet/V.png',
+  },
+  {
+    name: 'W',
+    src: '/alphabet/W.png',
+  },
+  {
+    name: 'X',
+    src: '/alphabet/X.png',
+  },
+  {
+    name: 'Y',
+    src: '/alphabet/Y.png',
+  },
+  {
+    name: 'Z',
+    src: '/alphabet/Z.png',
+  },
+]
