97c4d69dee5923f03ae333facbcb93e37236bafb
zimmer <zimmertorrijos@gmail.com>
2024-06-08 01:16:23 +0800

    update default page to gesture to text

--- apps/src/pages/Index.vue
+++ apps/src/pages/Index.vue
@@ -64,7 +64,7 @@ enum Mode {
 }
 
 const hasModel = ref(true)
-const mode = ref<Mode>(hasModel.value ? Mode.settings : Mode.loading)
+const mode = ref<Mode>(hasModel.value ? Mode.gesture_to_text : Mode.loading)
 const lastScannerMode = ref(0)
 
 const menu = ref({})
